de3b9c325041a4ebd2f25e9967d89c24610579be
holicz <phatapahk.pirat@gmail.com>
2019-04-24 13:33:41 +0700

    singleton pattern implement

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/* 	singleton pattern : create instance once time per executed  */
+
+type singleton struct {}
+
+var (
+	once     sync.Once
+	instance *singleton
+)
+
+func newInstance() *singleton {
+	// do one time when program executed.
+	once.Do(func() {
+		instance = &singleton{}
+	})
+	// return created instance
+	return instance
+}
+
+func main() {
+	instance1  := newInstance()
+	instance2  := newInstance()
+
+	fmt.Print(instance1==instance2)
+}
